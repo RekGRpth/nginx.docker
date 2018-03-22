c7a154916a67b4c28147b289b9ffd4afc464ab08
RekGRpth <rekgrpth@gmail.com>
2018-03-22 10:02:23 +0500

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ "$GROUP_ID" = "" ]; then GROUP_ID=$(id -g $GROUP); fi
-if [ "$GROUP_ID" != "$(id -g $GROUP)" ]; then
-    find / -group $GROUP -exec chgrp "$GROUP_ID" {} \;
-    groupmod --gid "$GROUP_ID" $GROUP
+if [ "$GROUP_ID" = "" ]; then GROUP_ID=$(id -g "$GROUP"); fi
+if [ "$GROUP_ID" != "$(id -g "$GROUP")" ]; then
+    find / -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
+    groupmod --gid "$GROUP_ID" "$GROUP"
 fi
 
-if [ "$USER_ID" = "" ]; then USER_ID=$(id -u $USER); fi
-if [ "$USER_ID" != "$(id -u $USER)" ]; then
-    find / -user $USER -exec chown "$USER_ID" {} \;
-    usermod --uid "$USER_ID" $USER
+if [ "$USER_ID" = "" ]; then USER_ID=$(id -u "$USER"); fi
+if [ "$USER_ID" != "$(id -u "$USER")" ]; then
+    find / -user "$USER" -exec chown "$USER_ID" {} \;
+    usermod --uid "$USER_ID" "$USER"
 fi
 
 #sed -i "/^\tinclude \/etc\/nginx\/conf\.d\/\*\.conf/cinclude \/data\/\*\/nginx\.conf;" "/etc/nginx/nginx.conf"
@@ -18,5 +18,8 @@ fi
 sed -i "/^worker_processes/cworker_processes 4;" "/etc/nginx/nginx.conf"
 
 mkdir -p /run/nginx
+
+find "$HOME" ! -group "$GROUP" -exec chgrp "$GROUP_ID" {} \;
+find "$HOME" ! -user "$USER" -exec chown "$USER_ID" {} \;
 
 exec "$@"
