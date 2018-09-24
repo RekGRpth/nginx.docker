5638ad727e3a8d887c35df5dfd264c76c82cca6c
RekGRpth <rekgrpth@gmail.com>
2018-09-24 16:10:05 +0500

    up

--- run.sh
+++ run.sh
@@ -18,17 +18,17 @@ docker run \
     --publish 443:443 \
     --publish 80:80 \
     --restart always \
-    --volume /etc/certs/`hostname -d`.crt:/etc/nginx/ssl/`hostname -d`.crt:ro \
-    --volume /etc/certs/`hostname -d`.key:/etc/nginx/ssl/`hostname -d`.key:ro \
+    --volume /etc/certs/`hostname -d`.crt:/etc/nginx/ssl/`hostname -d`.crt \
+    --volume /etc/certs/`hostname -d`.key:/etc/nginx/ssl/`hostname -d`.key \
     --volume nginx:/data/nginx \
     $(find /var/lib/docker/volumes -maxdepth 1 -mindepth 1 -type d | while read VOLUME; do
         if test -n "$(docker ps --filter "name=$(basename "$VOLUME")" --filter "status=running" --format "{{.Names}}")"; then
-            test -f "$VOLUME/_data/nginx.conf" && echo "--volume $VOLUME/_data/nginx.conf:/etc/nginx/conf.d/$(basename "$VOLUME").conf:ro"
-            test -d "$VOLUME/_data/app" && echo "--volume $VOLUME/_data/app:/data/$(basename "$VOLUME"):ro"
+            test -f "$VOLUME/_data/nginx.conf" && echo "--volume $VOLUME/_data/nginx.conf:/etc/nginx/conf.d/$(basename "$VOLUME").conf"
+            test -d "$VOLUME/_data/app" && echo "--volume $VOLUME/_data/app:/data/$(basename "$VOLUME")"
             test -d "$VOLUME/_data/log" && echo "--volume $VOLUME/_data/log:/var/log/nginx/$(basename "$VOLUME")"
         fi
     done) \
-    --volume /var/lib/docker/volumes/nginx/_data/module.conf:/etc/nginx/modules/nginx.conf:ro \
-    --volume /var/lib/docker/volumes/nginx/_data/nginx.conf:/etc/nginx/conf.d/nginx.conf:ro \
+    --volume /var/lib/docker/volumes/nginx/_data/module.conf:/etc/nginx/modules/nginx.conf \
+    --volume /var/lib/docker/volumes/nginx/_data/nginx.conf:/etc/nginx/conf.d/nginx.conf \
     --volume /var/lib/docker/volumes/nginx/_data/log:/var/log/nginx/nginx \
     rekgrpth/nginx
