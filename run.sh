2d11e4fba1b3533b45e29d534d13025c9a5c448c
RekGRpth <rekgrpth@gmail.com>
2019-07-03 10:27:38 +0500

    up

--- run.sh
+++ run.sh
@@ -8,9 +8,10 @@ docker pull rekgrpth/nginx || exit $?
 docker volume create nginx || exit $?
 docker network create my
 mkdir -p /var/lib/docker/volumes/nginx/_data/log
+touch /var/lib/docker/volumes/nginx/_data/http.conf
+touch /var/lib/docker/volumes/nginx/_data/main.conf
 touch /var/lib/docker/volumes/nginx/_data/module.conf
 touch /var/lib/docker/volumes/nginx/_data/nginx.conf
-touch /var/lib/docker/volumes/nginx/_data/common.conf
 docker run \
     --detach \
     --env GROUP_ID=$(id -g) \
@@ -29,12 +30,13 @@ docker run \
         if test -n "$(docker ps --filter "name=$(basename "$VOLUME")" --filter "status=running" --format "{{.Names}}")"; then
             test -d "$VOLUME/_data/app" && echo "--volume $VOLUME/_data/app:/data/$(basename "$VOLUME")"
             test -d "$VOLUME/_data/log" && echo "--volume $VOLUME/_data/log:/var/log/nginx/$(basename "$VOLUME")"
-            test -f "$VOLUME/_data/nginx.conf" && echo "--volume $VOLUME/_data/nginx.conf:/etc/nginx/conf.d/$(basename "$VOLUME").conf"
             test -f "$VOLUME/_data/nginx.conf" && echo "--link nginx:$(basename "$VOLUME")-$(hostname -f)"
+            test -f "$VOLUME/_data/nginx.conf" && echo "--volume $VOLUME/_data/nginx.conf:/etc/nginx/conf.d/$(basename "$VOLUME").conf"
         fi
     done) \
+    --volume /var/lib/docker/volumes/nginx/_data/http.conf:/etc/nginx/conf.d/___common.conf \
     --volume /var/lib/docker/volumes/nginx/_data/log:/var/log/nginx/nginx \
+    --volume /var/lib/docker/volumes/nginx/_data/main.conf:/etc/nginx/modules/___common.conf \
     --volume /var/lib/docker/volumes/nginx/_data/module.conf:/etc/nginx/modules/nginx.conf \
     --volume /var/lib/docker/volumes/nginx/_data/nginx.conf:/etc/nginx/conf.d/nginx.conf \
-    --volume /var/lib/docker/volumes/nginx/_data/common.conf:/etc/nginx/conf.d/___common.conf \
     rekgrpth/nginx
