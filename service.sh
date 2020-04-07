e65e3dffc0b5a7bb9dd55a87a18a932fdaeab887
RekGRpth <rekgrpth@gmail.com>
2020-04-07 08:20:23 +0500

    up

--- service.sh
+++ service.sh
@@ -10,11 +10,12 @@ touch /var/lib/docker/volumes/nginx/_data/http.conf
 touch /var/lib/docker/volumes/nginx/_data/main.conf
 touch /var/lib/docker/volumes/nginx/_data/module.conf
 docker service create \
+    --constraint node.platform.os==linux \
     --env GROUP_ID=$(id -g) \
     --env LANG=ru_RU.UTF-8 \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID=$(id -u) \
-    --hostname nginx \
+    --mode global \
     --mount type=bind,source=/etc/certs,destination=/etc/certs \
     --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
     --mount type=bind,source=/run/uwsgi,destination=/run/uwsgi \
@@ -22,10 +23,10 @@ docker service create \
     --mount type=volume,source=nginx,destination=/home \
     --name nginx \
     --publish target=443,published=443,mode=host \
-    --network name=docker,alias=$(hostname -f),alias=api-$(hostname -f),alias=cas-$(hostname -f)$(find /var/lib/docker/volumes -maxdepth 1 -mindepth 1 -type d | while read VOLUME; do
-        echo -n ",alias=$(basename "$VOLUME")-$(hostname -f)"
+    --network name=docker,alias=$(hostname -f),alias=api-$(hostname -f),alias=cas-$(hostname -f)$(docker volume ls --format "{{.Name}}" | while read VOLUME; do
+        echo -n ",alias=$VOLUME-$(hostname -f)"
     done) \
-    $(find /var/lib/docker/volumes -maxdepth 1 -mindepth 1 -type d | while read VOLUME; do
-        echo "--mount type=bind,source=$VOLUME/_data,destination=/etc/nginx/$(basename "$VOLUME")"
+    $(docker volume ls --format "{{.Name}}" | while read VOLUME; do
+        echo "--mount type=volume,source=$VOLUME,destination=/etc/nginx/$VOLUME"
     done) \
     rekgrpth/nginx
