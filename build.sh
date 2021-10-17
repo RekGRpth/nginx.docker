ae88889241ad444e7fe1fad704dc7bd85ffe277e
RekGRpth <rekgrpth@gmail.com>
2021-10-17 09:39:35 +0500

    edge

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-DOCKER_BUILDKIT=1 docker build --progress=plain --tag rekgrpth/nginx . 2>&1 | tee build.log
+DOCKER_BUILDKIT=1 docker build --progress=plain --tag ghcr.io/rekgrpth/nginx.docker . 2>&1 | tee build.log
