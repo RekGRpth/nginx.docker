edec2ce586b731cc95328d6977466474c4957450
RekGRpth <rekgrpth@gmail.com>
2022-01-17 16:09:20 +0500

    fix

--- bin/docker_test_apt.sh
+++ bin/docker_test_apt.sh
@@ -21,6 +21,7 @@ cpan -Ti \
 ;
 PG_VERSION="$(pg_lsclusters --no-header | cut -f1 -d ' ')"
 cat >"/etc/postgresql/$PG_VERSION/main/pg_hba.conf" <<EOF
+local   all             postgres                                trust
 local   all             all                                     trust
 host    all             all             127.0.0.1/32            trust
 host    all             all             ::1/128                 trust
