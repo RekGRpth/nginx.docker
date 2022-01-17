cbc5f59023614d1a99eb71fb5d72aa5e1ded449f
RekGRpth <rekgrpth@gmail.com>
2022-01-17 15:23:58 +0500

    fix

--- bin/docker_test_apt.sh
+++ bin/docker_test_apt.sh
@@ -20,14 +20,15 @@ cpan -Ti \
     Test::Nginx::Socket \
 ;
 PG_VERSION="$(pg_lsclusters --no-header | cut -f1 -d ' ')"
-ln -fs /dev/stdout "/var/log/postgresql/postgresql-$PG_VERSION-main.log"
-ln -fs /dev/stderr "/var/log/postgresql/postgresql-$PG_VERSION-main.log"
+#ln -fs /dev/stdout "/var/log/postgresql/postgresql-$PG_VERSION-main.log"
+#ln -fs /dev/stderr "/var/log/postgresql/postgresql-$PG_VERSION-main.log"
 PGDATA="/var/lib/postgresql/$PG_VERSION/main"
 cat >>"$PGDATA/pg_hba.conf" <<EOF
 host all all samenet trust
 host replication all samenet trust
 EOF
 cat >>"$PGDATA/postgresql.auto.conf" <<EOF
-listen_addresses = '*'
+log_destination = 'stderr'
+unix_socket_directories = '/run/postgresql,/tmp'
 EOF
 service postgresql start
