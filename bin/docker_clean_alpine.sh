f036440fa8ec614a37385dc028f43c56a27444a9
RekGRpth <rekgrpth@gmail.com>
2022-01-19 18:34:55 +0500

    fix

--- bin/docker_clean_alpine.sh
+++ bin/docker_clean_alpine.sh
@@ -3,7 +3,7 @@
 cd /
 apk add --no-cache --virtual .nginx-rundeps \
     apache2-utils \
-    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | while read -r lib; do test ! -e "/usr/local/lib/$lib" && echo "so:$lib"; done) \
+    $(scanelf --needed --nobanner --format '%n#p' --recursive /usr/local | tr ',' '\n' | sort -u | while read -r lib; do test -z "$(find /usr/local/lib -name "$lib")" && echo "so:$lib"; done) \
 ;
 find /usr/local/bin -type f -exec strip '{}' \;
 find /usr/local/lib -type f -name "*.so" -exec strip '{}' \;
