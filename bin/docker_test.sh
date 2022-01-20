4cf56cc160713fd708ab043e6119c3615eec2bd2
RekGRpth <rekgrpth@gmail.com>
2022-01-20 09:15:44 +0500

    fix

--- bin/docker_test.sh
+++ bin/docker_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 install -d -m 1775 -o postgres -g postgres /run/postgresql /var/log/postgresql
-gosu postgres pg_ctl initdb --auth=trust --data-checksums --pgdata=/var/lib/postgresql/data
+gosu postgres initdb --auth=trust --data-checksums --pgdata=/var/lib/postgresql/data
 gosu postgres pg_ctl -w start --pgdata=/var/lib/postgresql/data
 cd "$HOME"
 find "$HOME/src/nginx/modules" -type d -name "t" | grep -v "\.git" | sort | while read -r NAME; do
