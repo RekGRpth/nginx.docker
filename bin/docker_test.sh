c79f9d7fe414009e73c72dcc833e385339b96f0a
RekGRpth <rekgrpth@gmail.com>
2022-01-20 09:44:33 +0500

    fix

--- bin/docker_test.sh
+++ bin/docker_test.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -eux
 
 install -d -m 1775 -o postgres -g postgres /run/postgresql /var/log/postgresql
-INITDB="$(which initdb)"
-PG_CTL="$(which pg_ctl)"
-gosu postgres "$INITDB" --auth=trust --pgdata=/var/lib/postgresql/data
-gosu postgres "$PG_CTL" -w start --pgdata=/var/lib/postgresql/data
+DOCKER_TYPE="$(cat /etc/os-release | grep -E '^ID=' | cut -f2 -d '=')"
+if [ $DOCKER_TYPE != "alpine" ]; then
+    POSTGRES_VERSION="$(pg_lsclusters --no-header | cut -f1 -d ' ')"
+    export PATH="$PATH:/usr/lib/postgresql/$POSTGRES_VERSION/bin"
+fi
+gosu postgres initdb --auth=trust --pgdata=/var/lib/postgresql/data
+gosu postgres pg_ctl -w start --pgdata=/var/lib/postgresql/data
 cd "$HOME"
 find "$HOME/src/nginx/modules" -type d -name "t" | grep -v "\.git" | sort | while read -r NAME; do
     cd "$(dirname "$NAME")" && prove
 done
-gosu postgres "$PG_CTL" -m fast -w stop --pgdata=/var/lib/postgresql/data
+gosu postgres pg_ctl -m fast -w stop --pgdata=/var/lib/postgresql/data
