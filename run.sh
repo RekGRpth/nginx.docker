15bb3cfcf57a414929df225ef79c2e39c543ca66
RekGRpth <rekgrpth@gmail.com>
2022-02-21 09:50:31 +0500

    rename

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ docker network create --attachable --opt com.docker.network.bridge.name=docker d
 docker volume create nginx
 mkdir -p /var/lib/docker/volumes/nginx/_data/log
 touch /var/lib/docker/volumes/nginx/_data/http.conf
-touch /var/lib/docker/volumes/nginx/_data/main.conf
+touch /var/lib/docker/volumes/nginx/_data/nginx.conf
 touch /var/lib/docker/volumes/nginx/_data/module.conf
 docker stop nginx || echo $?
 docker rm nginx || echo $?
@@ -21,7 +21,7 @@ docker run \
     --mount type=bind,source=/run/nginx,destination=/run/nginx \
     --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
     --mount type=bind,source=/run/uwsgi,destination=/run/uwsgi \
-    --mount type=bind,source=/var/lib/docker/volumes/nginx/_data/main.conf,destination=/etc/nginx/nginx.conf,readonly \
+    --mount type=bind,source=/var/lib/docker/volumes/nginx/_data/nginx.conf,destination=/etc/nginx/nginx.conf,readonly \
     --mount type=bind,source=/var/log/nginx,destination=/var/log/nginx \
     --mount type=volume,source=nginx,destination=/var/cache/nginx \
     --name nginx \
