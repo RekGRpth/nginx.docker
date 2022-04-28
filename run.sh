aaa4c1c493e4c50bd278449c9837ac0db4c99db1
RekGRpth <rekgrpth@gmail.com>
2022-04-28 08:13:28 +0500

    conf

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker pull "ghcr.io/rekgrpth/nginx.docker:${INPUTS_BRANCH:-latest}"
 docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
 docker volume create nginx
 NGINX="$(docker volume inspect --format "{{ .Mountpoint }}" nginx)"
+touch "$NGINX/nginx.conf"
 docker stop nginx || echo $?
 docker rm nginx || echo $?
 docker run \
@@ -14,6 +15,7 @@ docker run \
     --env TZ=Asia/Yekaterinburg \
     --env USER_ID="$(id -u)" \
     --hostname nginx \
+    --mount type=bind,source="$NGINX/nginx.conf",destination=/etc/nginx/nginx.conf,readonly \
     --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
     --mount type=bind,source=/run/nginx,destination=/run/nginx \
     --mount type=bind,source=/run/postgresql,destination=/run/postgresql \
